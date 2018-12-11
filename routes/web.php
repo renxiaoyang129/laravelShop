8ea8415bb4118cc022ffc7f7cae5520ec5c214e5
RxyLaravelShop <1294657476@qq.com>
2018-12-11 17:06:23 +0800

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,10 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('cart', 'CartController@index')->name('cart.index');
 
         Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+
+        //订单
+        Route::post('orders', 'OrdersController@store')->name('orders.store');
+
     });
 
     Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
